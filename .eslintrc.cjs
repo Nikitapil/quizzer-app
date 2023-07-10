1512345e92ac1f5d09acdd1f6874843b3c939047
Nikitapil <nikitapiliavets@gmail.com>
2023-07-10 19:42:01 +0300

    add app core

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: ['prettier'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-setup-props-destructure': 'off',
+    "vue/attributes-order": ["error", {
+      "order": [
+        "DEFINITION",
+        "LIST_RENDERING",
+        "CONDITIONALS",
+        "RENDER_MODIFIERS",
+        "GLOBAL",
+        ["UNIQUE", "SLOT"],
+        "TWO_WAY_BINDING",
+        "OTHER_DIRECTIVES",
+        "OTHER_ATTR",
+        "EVENTS",
+        "CONTENT"
+      ],
+      "alphabetical": false
+    }]
+  },
+
+}
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p :style="{ fontWeight: 700 }">BOLD</p>
+    <p :style="{ fontWeight: 600 }">SEMI-BOLD</p>
+    <p :style="{ fontWeight: 400 }">Whereas recognition of the inherent dignity</p>
+  </div>
+</template>
