2c08b58f907994f68f263e0e5eb512162f97e9be
Nikitapil <nikitapiliavets@gmail.com>
2023-07-10 21:29:31 +0300

    setup i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,23 @@ import './assets/styles/main.scss';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
+import messages from './locales/index';
 
 import App from './App.vue';
 import router from './router';
+
+const i18n = createI18n({
+  availableLocales: ['ru', 'en'],
+  locale: 'en',
+  fallbackLocale: 'ru',
+  messages
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount('#app');
