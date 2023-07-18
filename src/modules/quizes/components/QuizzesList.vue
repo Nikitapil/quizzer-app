e5ce07c685c8f49c55fa4113105c3928319f7802
Nikitapil <nikitapiliavets@gmail.com>
2023-07-18 23:51:02 +0300

    extract quizzes list component

---
+++ src/modules/quizes/components/QuizzesList.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <h1 class="title">{{ title }}</h1>
+    <form
+      v-if="isShowSearch"
+      class="controls"
+      @submit.prevent
+    >
+      <AppInput
+        v-model="search"
+        :placeholder="$t('search')"
+      />
+      <AppButton
+        type="submit"
+        :text="$t('search')"
+        @click="onSearch"
+      />
+    </form>
+    <div
+      v-if="store.isQuizzesLoading"
+      class="loader"
+    >
+      <RoundLoader />
+    </div>
+    <div v-else-if="!store.quizzes.length">{{ $t('no_quizzes_yet') }}.</div>
+    <div
+      v-else
+      class="quizzes-container"
+    >
+      <QuizzesListItem
+        v-for="quiz in store.quizzes"
+        :key="quiz.id"
+        :quiz="quiz"
+      />
+      <Pagination
+        :total-items-count="store.totalCount"
+        :limit="10"
+        :current-page="page"
+        @change-page="onChangePage"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+import { useQuizzesStore } from '@/modules/quizes/store/QuizzesStore';
+import RoundLoader from '@/components/loaders/RoundLoader.vue';
+import QuizzesListItem from '@/modules/quizes/components/QuizzesListItem.vue';
+import Pagination from '@/components/Pagination.vue';
+import AppInput from '@/components/inputs/AppInput.vue';
+import AppButton from '@/components/AppButton.vue';
+import { IGetQuizzesRequest } from '@/modules/quizes/domain/types';
+
+defineProps<{
+  title: string;
+}>();
+
+const emit = defineEmits<{
+  'get-quizzes': [quizParams: IGetQuizzesRequest];
+}>();
+
+const store = useQuizzesStore();
+
+const page = ref(1);
+const search = ref('');
+
+const getQuizzes = async () => {
+  emit('get-quizzes', {
+    page: page.value,
+    search: search.value
+  });
+};
+
+const onSearch = () => {
+  page.value = 1;
+  getQuizzes();
+};
+
+const onChangePage = (p: number) => {
+  page.value = p;
+  getQuizzes();
+};
+
+const isShowSearch = computed(() => {
+  return !store.isQuizzesLoading && (!!store.quizzes.length || !!search.value);
+});
+
+onMounted(() => {
+  getQuizzes();
+});
+</script>
+
+<style scoped>
+.title {
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: 16px;
+}
+
+.loader {
+  margin: 0 auto;
+  width: fit-content;
+}
+
+.quizzes-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  gap: 16px;
+}
+
+.controls {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  margin: 0 auto;
+  margin-bottom: 16px;
+  max-width: 800px;
+}
+</style>
--- src/modules/quizes/views/AllQuizes.vue
+++ src/modules/quizes/views/AllQuizes.vue
@@ -1,45 +1,8 @@
 <template>
-  <div>
-    <h1 class="title">{{ $t('all_quizzes') }}</h1>
-    <form
-      v-if="isShowSearch"
-      class="controls"
-      @submit.prevent
-    >
-      <AppInput
-        v-model="search"
-        :placeholder="$t('search')"
-      />
-      <AppButton
-        type="submit"
-        :text="$t('search')"
-        @click="onSearch"
-      />
-    </form>
-    <div
-      v-if="store.isQuizzesLoading"
-      class="loader"
-    >
-      <RoundLoader />
-    </div>
-    <div v-else-if="!store.quizzes.length">{{ $t('no_quizzes_yet') }}.</div>
-    <div
-      v-else
-      class="quizzes-container"
-    >
-      <QuizzesListItem
-        v-for="quiz in store.quizzes"
-        :key="quiz.id"
-        :quiz="quiz"
-      />
-      <Pagination
-        :total-items-count="store.totalCount"
-        :limit="10"
-        :current-page="page"
-        @change-page="onChangePage"
-      />
-    </div>
-  </div>
+  <QuizzesList
+    :title="$t('all_quizzes')"
+    @get-quizzes="store.getAllQuizes"
+  />
 </template>
 
 <script setup lang="ts">
@@ -47,75 +10,12 @@ import { useBreadCrumbs } from '@/composables/useBreadCrumbs';
 import { BREADCRUMBS } from '@/constants/breadcrumbs';
 import { useDocTitle } from '@/composables/useDocTitle';
 import { useI18n } from 'vue-i18n';
-import { computed, onMounted, ref } from 'vue';
 import { useQuizzesStore } from '@/modules/quizes/store/QuizzesStore';
-import RoundLoader from '@/components/loaders/RoundLoader.vue';
-import QuizzesListItem from '@/modules/quizes/components/QuizzesListItem.vue';
-import Pagination from '@/components/Pagination.vue';
-import AppInput from '@/components/inputs/AppInput.vue';
-import AppButton from '@/components/AppButton.vue';
+import QuizzesList from '@/modules/quizes/components/QuizzesList.vue';
 
 const { t } = useI18n();
 useBreadCrumbs([BREADCRUMBS.MAIN, BREADCRUMBS.ALL_QUIZZES]);
 useDocTitle(t('all_quizzes'));
 
 const store = useQuizzesStore();
-
-const page = ref(1);
-const search = ref('');
-
-const getQuizzes = async () => {
-  await store.getAllQuizes({
-    page: page.value,
-    search: search.value
-  });
-};
-
-const onSearch = async () => {
-  page.value = 1;
-  await getQuizzes();
-};
-
-const onChangePage = async (p: number) => {
-  page.value = p;
-  await getQuizzes();
-};
-
-const isShowSearch = computed(() => {
-  return !store.isQuizzesLoading && (!!store.quizzes.length || !!search.value);
-});
-
-onMounted(async () => {
-  await getQuizzes();
-});
 </script>
-
-<style scoped>
-.title {
-  font-size: 32px;
-  text-align: center;
-  margin-bottom: 16px;
-}
-
-.loader {
-  margin: 0 auto;
-  width: fit-content;
-}
-
-.quizzes-container {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  align-items: center;
-  gap: 16px;
-}
-
-.controls {
-  display: flex;
-  gap: 8px;
-  align-items: center;
-  margin: 0 auto;
-  margin-bottom: 16px;
-  max-width: 800px;
-}
-</style>
