99a0712652c92c95358bad9404eca5ecf0b2d265
Nikitapil <nikitapiliavets@gmail.com>
2023-07-19 20:05:14 +0300

    add more games link

--- src/modules/app/App.vue
+++ src/modules/app/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
+  <div class="app">
     <AppHeader />
     <div class="panel">
       <BreadCrumbs :breabcrums="store.breadCrumbs" />
       <lang-switcher />
     </div>
-    <main class="container">
+    <main class="container main">
       <router-view />
     </main>
+    <AppFooter />
   </div>
 </template>
 <script setup lang="ts">
@@ -15,14 +16,21 @@ import AppHeader from '@/modules/app/components/AppHeader.vue';
 import BreadCrumbs from '@/modules/app/components/BreadCrumbs.vue';
 import LangSwitcher from '@/modules/app/components/LangSwitcher.vue';
 import { useAppStore } from '@/modules/app/store/AppStore';
+import AppFooter from '@/modules/app/components/AppFooter.vue';
 const store = useAppStore();
 </script>
 <style lang="scss" scoped>
+@import '../../assets/styles/vars';
+
 .panel {
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 5px 10px;
+}
+
+.main {
+  min-height: $main-height;
 }
 </style>
---
+++ src/modules/app/components/AppFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <a
+      class="footer__link"
+      href="https://nikitapil.github.io/gamelandia/"
+      target="_blank"
+    >
+      More games here
+    </a>
+  </footer>
+</template>
+<style scoped lang="scss">
+@import '../../../assets/styles/colors';
+
+.footer {
+  padding: 10px;
+
+  &__link {
+    display: block;
+    width: fit-content;
+    font-size: 16px;
+    text-decoration: none;
+    color: inherit;
+    margin-left: auto;
+    transition: 0.4s;
+
+    &:hover {
+      text-shadow: $text-shadow-thite-big;
+    }
+  }
+}
+</style>
