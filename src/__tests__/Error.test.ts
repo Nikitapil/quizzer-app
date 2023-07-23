7e88d6d36e28a5f5c75ddca499e3f8c084a5b7cd
Nikitapil <nikitapiliavets@gmail.com>
2023-07-23 21:57:42 +0300

    add Error page test

---
+++ src/__tests__/Error.test.ts
@@ -0,0 +1,19 @@
+import { mount } from '@vue/test-utils';
+import App from '@/modules/app/App.vue';
+import { i18n } from '@/main';
+import router from '@/router';
+
+describe('Error page tests', () => {
+  it('should redirect to error page', async () => {
+    const wrapper = mount(App, {
+      global: {
+        plugins: [i18n, router]
+      }
+    });
+
+    await router.push('/not_existed_route');
+
+    expect(wrapper.find('.error-page').exists()).toBeTruthy();
+    expect(document.title).toBe('Quizzer | 404');
+  });
+});
