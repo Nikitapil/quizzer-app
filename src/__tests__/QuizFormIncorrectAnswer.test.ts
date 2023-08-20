50c49032e032dfa2118cd1203d7d70eee89327ac
Nikitapil <nikitapiliavets@gmail.com>
2023-08-20 22:15:18 +0300

    add QuizFormIncorrectAnswer tests

---
+++ src/__tests__/QuizFormIncorrectAnswer.test.ts
@@ -0,0 +1,79 @@
+import { mount } from '@vue/test-utils';
+import QuizFormIncorrectAnswer from '@/modules/quizes/components/quiz-form/QuizFormIncorrectAnswer.vue';
+import { i18n } from '@/main';
+import AppButton from '@/components/AppButton.vue';
+import AppInput from '@/components/inputs/AppInput.vue';
+
+describe('QuizFormIncorrectAnswer component tests', () => {
+  it('should render answer without delete btn if 0 index', () => {
+    const wrapper = mount(QuizFormIncorrectAnswer, {
+      props: {
+        index: 0,
+        questionId: '123',
+        isLoading: false
+      },
+      global: {
+        plugins: [i18n]
+      }
+    });
+
+    const deleteBtn = wrapper.find('button');
+
+    expect(deleteBtn.exists()).toBe(false);
+  });
+
+  it('should render answer with delete btn if index more than 0', () => {
+    const wrapper = mount(QuizFormIncorrectAnswer, {
+      props: {
+        index: 1,
+        questionId: '123',
+        isLoading: false
+      },
+      global: {
+        plugins: [i18n]
+      }
+    });
+
+    const deleteBtn = wrapper.find('button');
+
+    expect(deleteBtn.exists()).toBe(true);
+  });
+
+  it('should disable input and btn if isLoading', () => {
+    const wrapper = mount(QuizFormIncorrectAnswer, {
+      props: {
+        index: 1,
+        questionId: '123',
+        isLoading: true
+      },
+      global: {
+        plugins: [i18n]
+      }
+    });
+
+    const deleteBtn = wrapper.getComponent(AppInput);
+    const input = wrapper.getComponent(AppButton);
+
+    expect(deleteBtn.props().disabled).toBe(true);
+    expect(input.props().disabled).toBe(true);
+  });
+
+  it('should emit delete-answer event', async () => {
+    const wrapper = mount(QuizFormIncorrectAnswer, {
+      props: {
+        index: 1,
+        questionId: '123',
+        isLoading: false
+      },
+      global: {
+        plugins: [i18n]
+      }
+    });
+
+    const deleteBtn = wrapper.getComponent(AppButton);
+
+    await deleteBtn.trigger('click');
+
+    expect(wrapper.emitted('delete-answer')).toBeTruthy();
+  });
+});
