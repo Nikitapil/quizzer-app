0a67bfcf61fcdaf3962b9dcbac5195620ab58d18
Nikitapil <nikitapiliavets@gmail.com>
2023-08-29 23:06:36 +0300

    add useAuthRedirect test

--- src/__tests__/Composables.test.ts
+++ src/__tests__/Composables.test.ts
@@ -1,4 +1,10 @@
 import { useDocTitle } from '@/composables/useDocTitle';
+import { flushPromises, mount } from '@vue/test-utils';
+import Profile from '@/modules/profile/views/Profile.vue';
+import { i18n } from '@/main';
+import { createTestingPinia } from '@pinia/testing';
+import router from '@/router';
+import { ERoutesNames } from '@/router/routes-names';
 
 describe('Composables', () => {
   it('should work useDocTitle composable', () => {
@@ -7,5 +13,17 @@ describe('Composables', () => {
     useDocTitle('Hello');
 
     expect(document.title).toBe('Quizzer | Hello');
+  });
+
+  it('should work useAuthRedirect', async () => {
+    mount(Profile, {
+      global: {
+        plugins: [i18n, createTestingPinia(), router]
+      }
+    });
+
+    await flushPromises();
+
+    expect(router.currentRoute.value.name).toBe(ERoutesNames.SIGN_IN);
   });
 });
