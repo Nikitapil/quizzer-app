dff5961c548336a983d53f1584be6cab1494bed1
Nikitapil <nikitapiliavets@gmail.com>
2024-06-29 22:39:03 +0300

    refactor QuizFormStore

--- src/modules/quizes/store/QuizFormStore.ts
+++ src/modules/quizes/store/QuizFormStore.ts
@@ -1,76 +1,67 @@
-import { defineStore } from 'pinia';
+import { useApiMethod } from '@/api/useApiMethod';
+import { quizApi } from '@/api/apiInstances';
+import { computed, ref } from 'vue';
 import type {
   IGetQuizFormParams,
-  IQuizFormStoreActions,
-  IQuizFormStoreState,
   IQuizFormValues,
-  TQuizFormStoreGetters
+  TEditQuizParams
 } from '@/modules/quizes/domain/types';
-import { toast } from 'vue3-toastify';
-import { QuizzesService } from '@/modules/quizes/QuizzesService';
+import type { CreateQuizDto } from '@/api/swagger/Quizes/data-contracts';
 import { mapQuizResponseToQuizForm } from '@/modules/quizes/helpers/mapers';
+import { defineStore } from 'pinia';
 
-export const useQuizFormStore = defineStore<
-  'quiz-form',
-  IQuizFormStoreState,
-  TQuizFormStoreGetters,
-  IQuizFormStoreActions
->('quiz-form', {
-  state: () => {
-    return {
-      isLoading: false,
-      quizForm: null,
-      isQuizLoading: false,
-      quizId: ''
-    };
-  },
+export const useQuizFormStore = defineStore('quizFormStore', () => {
+  const quizForm = ref<IQuizFormValues | null>();
+  const isQuizLoading = ref<boolean>(false);
+  const quizId = ref<string>('');
 
-  actions: {
-    async createQuiz(data: IQuizFormValues) {
-      try {
-        this.isLoading = true;
-        await QuizzesService.createQuiz(data);
-        return true;
-      } catch (e: any) {
-        if (e?.response?.data?.message) {
-          toast(e?.response.data.message);
-        }
-        return false;
-      } finally {
-        this.isLoading = false;
-      }
-    },
-    async editQuiz(data: IQuizFormValues) {
-      try {
-        this.isLoading = true;
-        await QuizzesService.editQuiz({ ...data, id: this.quizId });
-        return true;
-      } catch (e: any) {
-        if (e?.response?.data?.message) {
-          toast(e?.response.data.message);
-        }
-        return false;
-      } finally {
-        this.isLoading = false;
-      }
-    },
-    async getQuizForm({ quizId, userId }: IGetQuizFormParams) {
-      try {
-        this.isQuizLoading = true;
-        const { data } = await QuizzesService.getSingleQuiz(quizId);
-        if (data.userId !== userId) {
-          this.quizForm = null;
-          return;
-        }
-        this.quizForm = mapQuizResponseToQuizForm(data);
-        this.quizId = quizId;
-      } catch (e: any) {
-        if (e?.response?.data?.message) {
-          toast(e?.response.data.message);
-        }
-      } finally {
-        this.isQuizLoading = false;
-      }
+  const { call: createQuizApi, isLoading: isCreateQuizLoading } = useApiMethod(
+    quizApi.createQuiz
+  );
+
+  const { call: editQuizApi, isLoading: isEditQuizLoading } = useApiMethod(
+    quizApi.editQuiz
+  );
+
+  const { call: getQuiz } = useApiMethod(quizApi.getQuiz);
+
+  const isLoading = computed(
+    () => isCreateQuizLoading.value || isEditQuizLoading.value
+  );
+
+  const createQuiz = async (data: CreateQuizDto) => {
+    const isCreated = await createQuizApi(data);
+    return !!isCreated;
+  };
+
+  const editQuiz = async (data: TEditQuizParams) => {
+    const isEdited = await editQuizApi({ ...data, id: quizId.value });
+    return !!isEdited;
+  };
+
+  // TODO check on backend that quiz belongs to user
+  const getQuizForm = async ({ quizId: id, userId }: IGetQuizFormParams) => {
+    isQuizLoading.value = true;
+    const quiz = await getQuiz(id);
+
+    if (quiz?.userId !== userId || !quiz) {
+      quizForm.value = null;
+      isQuizLoading.value = false;
+      return;
     }
-  }
+
+    quizForm.value = mapQuizResponseToQuizForm(quiz);
+    quizId.value = id;
+    isQuizLoading.value = false;
+  };
+
+  return {
+    isLoading,
+    isQuizLoading,
+    quizForm,
+    quizId,
+    createQuiz,
+    editQuiz,
+    getQuizForm
+  };
 });
