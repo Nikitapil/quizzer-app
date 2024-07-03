67fedb56b0dfcc14f2cd86b6f48949f7ebee2ffc
Nikitapil <nikitapiliavets@gmail.com>
2024-07-03 21:29:51 +0300

    refactor AppCheckbox tests

--- src/__tests__/AppCheckbox.test.ts
+++ src/__tests__/AppCheckbox.test.ts
@@ -2,31 +2,22 @@ import { mount } from '@vue/test-utils';
 import AppCheckbox from '@/components/inputs/AppCheckbox.vue';
 
 describe('AppCheckbox tests', () => {
-  it('should render checkbox', () => {
-    const wrapper = mount(AppCheckbox, {
-      props: {
-        id: 'test-id',
-        label: 'Test label',
-        modelValue: false,
-        'onUpdate:modelValue': (e: any) => wrapper.setProps({ modelValue: e })
-      }
-    });
+  const wrapper = mount(AppCheckbox, {
+    props: {
+      id: 'test-id',
+      label: 'Test label',
+      modelValue: false,
+      'onUpdate:modelValue': (e: any) => wrapper.setProps({ modelValue: e })
+    }
+  });
 
+  it('should render checkbox', () => {
     const labelText = wrapper.get('[data-test="checkbox-label"]');
 
     expect(labelText.text()).toBe('Test label');
   });
 
   it('should change model value', async () => {
-    const wrapper = mount(AppCheckbox, {
-      props: {
-        id: 'test-id',
-        label: 'Test label',
-        modelValue: false,
-        'onUpdate:modelValue': (e: any) => wrapper.setProps({ modelValue: e })
-      }
-    });
-
     const input: any = wrapper.get('input');
 
     await input.setChecked();
