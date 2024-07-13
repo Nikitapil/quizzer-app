5a476cae75f6baf1840669f6ae2c01b7f3da83dd
Nikitapil <nikitapiliavets@gmail.com>
2024-07-13 17:42:18 +0300

    refactor Confirm modal tests

--- src/__tests__/ConfirmModal.test.ts
+++ src/__tests__/ConfirmModal.test.ts
@@ -3,158 +3,104 @@ import ConfirmModal from '@/components/ConfirmModal.vue';
 import AppButton from '@/components/AppButton.vue';
 
 describe('ConfirmModal component tests', () => {
-  it('should not render confirm modal if nodelvalue is false', () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: false,
-        isLoading: false,
-        title: 'Test model'
-      }
-    });
+  const titleText = 'Test model';
+  const modalSelector = '[data-test="modal"]';
+  const modalTitleSelector = '[data-test="modal-title"]';
+  const confirmTextSelector = '[data-test="confirm-text"]';
+  const cancelButtonSelector = '[data-test="cancel-btn"]';
+  const confirmButtonSelector = '[data-test="confirm-btn"]';
 
-    const modal = wrapper.find('[data-test="modal"]');
+  const props = {
+    modelValue: true,
+    isLoading: false,
+    title: titleText,
+    'onUpdate:modelValue': (e: boolean) => wrapper.setProps({ modelValue: e })
+  } as const;
+
+  const wrapper = mount(ConfirmModal, {
+    props
+  });
+
+  beforeEach(async () => {
+    await wrapper.setProps(props);
+  });
+
+  it('should not render confirm modal if no modelvalue is false', async () => {
+    await wrapper.setProps({ modelValue: false });
+
+    const modal = wrapper.find(modalSelector);
 
     expect(modal.exists()).toBe(false);
   });
 
   it('should render confirm modal with true modelvalue', () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model'
-      }
-    });
-
-    const modal = wrapper.find('[data-test="modal"]');
+    const modal = wrapper.find(modalSelector);
 
     expect(modal.exists()).toBe(true);
   });
 
   it('should render confirm modal with correct title', () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model'
-      }
-    });
+    const title = wrapper.get(modalTitleSelector);
 
-    const title = wrapper.get('[data-test="modal-title"]');
-
-    expect(title.text()).toBe('Test model');
+    expect(title.text()).toBe(titleText);
   });
 
   it('should not render text part if text is not provided', () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model'
-      }
-    });
-
-    const text = wrapper.find('[data-test="confirm-text"]');
+    const text = wrapper.find(confirmTextSelector);
 
     expect(text.exists()).toBe(false);
   });
 
-  it('should  render text part if text is provided', () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model',
-        text: 'Test model text'
-      }
-    });
+  it('should  render text part if text is provided', async () => {
+    const textContent = 'Test model text';
+    await wrapper.setProps({ text: textContent });
 
-    const text = wrapper.get('[data-test="confirm-text"]');
+    const text = wrapper.get(confirmTextSelector);
 
-    expect(text.text()).toBe('Test model text');
+    expect(text.text()).toBe(textContent);
   });
 
-  it('should disable buttons if isLoading', () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: true,
-        title: 'Test model',
-        text: 'Test model text'
-      }
-    });
+  it('should disable buttons if isLoading', async () => {
+    await wrapper.setProps({ isLoading: true });
 
     const buttons = wrapper.findAllComponents(AppButton);
 
     buttons
       .slice(1)
-      .forEach((button) => expect(button.props().disabled).toBe(true));
+      .forEach((button) => expect(button.props('disabled')).toBe(true));
   });
 
   it('should close modal on click cancel', async () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model',
-        text: 'Test model text',
-        'onUpdate:modelValue': (e: any) => wrapper.setProps({ modelValue: e })
-      }
-    });
+    await wrapper.setProps({ isLoading: false });
 
-    const cancelBtn = wrapper.get('[data-test="cancel-btn"]');
+    const cancelBtn = wrapper.get(cancelButtonSelector);
 
     await cancelBtn.trigger('click');
 
     await flushPromises();
 
-    const modal = wrapper.find('[data-test="modal"]');
+    const modal = wrapper.find(modalSelector);
 
     expect(modal.exists()).toBe(false);
   });
 
   it('should render confirm button with default text', async () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model'
-      }
-    });
-
-    const confirmBtn = wrapper.get('[data-test="confirm-btn"]');
+    const confirmBtn = wrapper.get(confirmButtonSelector);
 
     expect(confirmBtn.text()).toBe('Confirm');
   });
 
   it('should render confirm button with text from prop', async () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model',
-        confirmBtnText: 'Test'
-      }
-    });
+    const confirmBtnText = 'Test';
+    await wrapper.setProps({ confirmBtnText });
 
-    const confirmBtn = wrapper.get('[data-test="confirm-btn"]');
+    const confirmBtn = wrapper.get(confirmButtonSelector);
 
-    expect(confirmBtn.text()).toBe('Test');
+    expect(confirmBtn.text()).toBe(confirmBtnText);
   });
 
   it('should emit confirm event on click confirm btn', async () => {
-    const wrapper = mount(ConfirmModal, {
-      props: {
-        modelValue: true,
-        isLoading: false,
-        title: 'Test model',
-        confirmBtnText: 'Test'
-      }
-    });
-
-    const confirmBtn = wrapper.get('[data-test="confirm-btn"]');
-
-    expect(confirmBtn.text()).toBe('Test');
+    const confirmBtn = wrapper.get(confirmButtonSelector);
 
     await confirmBtn.trigger('click');
 
