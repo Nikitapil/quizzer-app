31f6fc60fe8d4f288d35336fbf0314aae7f258d3
Nikitapil <nikitapiliavets@gmail.com>
2024-07-17 22:41:14 +0300

    refactor App.vue

--- src/modules/app/App.vue
+++ src/modules/app/App.vue
@@ -1,31 +1,15 @@
-<template>
-  <div class="app">
-    <AppHeader />
-    <div class="panel">
-      <BreadCrumbs :breadcrumbs="store.breadcrumbs" />
-      <lang-switcher />
-    </div>
-    <main class="container main">
-      <div
-        v-if="authStore.isLoading"
-        class="centered-page"
-      >
-        <RoundLoader />
-      </div>
-      <router-view v-else />
-    </main>
-    <AppFooter />
-  </div>
-</template>
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
+import { useAppStore } from '@/modules/app/store/AppStore';
+import { useAuthStore } from '@/modules/auth/store/AuthStore';
+
 import AppHeader from '@/modules/app/components/app-header/AppHeader.vue';
 import BreadCrumbs from '@/modules/app/components/BreadCrumbs.vue';
 import LangSwitcher from '@/modules/app/components/LangSwitcher.vue';
-import { useAppStore } from '@/modules/app/store/AppStore';
 import AppFooter from '@/modules/app/components/AppFooter.vue';
-import { useAuthStore } from '@/modules/auth/store/AuthStore';
-import { onMounted } from 'vue';
 import RoundLoader from '@/components/loaders/RoundLoader.vue';
+
 const store = useAppStore();
 const authStore = useAuthStore();
 
@@ -33,6 +17,31 @@ onMounted(() => {
   authStore.refresh();
 });
 </script>
+
+<template>
+  <div class="app">
+    <AppHeader />
+
+    <div class="panel">
+      <BreadCrumbs :breadcrumbs="store.breadcrumbs" />
+      <lang-switcher />
+    </div>
+
+    <main class="container main">
+      <div
+        v-if="authStore.isLoading"
+        class="centered-page"
+      >
+        <RoundLoader />
+      </div>
+
+      <router-view v-else />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
+
 <style lang="scss" scoped>
 .panel {
   width: 100%;
