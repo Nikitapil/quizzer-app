87c9c12f166691e7041f2299fe2a5f68192d7369
Nikitapil <nikitapiliavets@gmail.com>
2024-08-03 00:23:40 +0300

    refactor HomeStore tests

--- src/__tests__/HomeStore.test.ts
+++ src/__tests__/HomeStore.test.ts
@@ -1,46 +1,49 @@
-import { createTestingPinia } from '@pinia/testing';
 import { useHomeStore } from '@/modules/home/store/HomeStore';
 import { flushPromises } from '@vue/test-utils';
-import { beforeAll } from 'vitest';
-import type { TestingPinia } from '@pinia/testing';
+import { vi } from 'vitest';
 import { quizApi } from '@/api/apiInstances';
+import { createPinia, setActivePinia } from 'pinia';
+import {
+  CategoryCountReturnDtoMock,
+  GenerateQuizDtoMock,
+  QuizCategoriesReturnDtoMock,
+  ReturnGeneratedQuizDtoMock
+} from '@/api/swagger/Quizes/mock';
+import {
+  mapCategoriesQuestionCounts,
+  mapCategoriesToOptions
+} from '@/modules/home/helpers/mappers';
+import { defaultQuestionsCountValue } from '@/modules/home/domain/constants';
 
 describe('HomeStore tests', () => {
-  const categories = [
-    {
-      id: 1,
-      name: 'category 1'
-    },
-    {
-      id: 2,
-      name: 'category 2'
-    }
-  ];
-
-  let pinia: TestingPinia;
   let store: ReturnType<typeof useHomeStore>;
+  const categoriesMock = QuizCategoriesReturnDtoMock.createMany(2);
+  const generateQuizResponseMock = ReturnGeneratedQuizDtoMock.create();
+  const categoriesQuestionCountResponseMock =
+    CategoryCountReturnDtoMock.create();
 
-  beforeAll(() => {
-    quizApi.getCategories = async () => categories;
-    pinia = createTestingPinia({
-      stubActions: false
-    });
+  const getCategoriesMock = vi.fn().mockResolvedValue(categoriesMock);
+  const generateQuizMock = vi.fn().mockResolvedValue(generateQuizResponseMock);
+  const getCategoriesQuestionCountMock = vi
+    .fn()
+    .mockResolvedValue(categoriesQuestionCountResponseMock);
 
-    store = useHomeStore(pinia);
+  quizApi.getCategories = getCategoriesMock;
+  quizApi.generateQuiz = generateQuizMock;
+  quizApi.getCategoriesQuestionCount = getCategoriesQuestionCountMock;
+
+  beforeEach(() => {
+    getCategoriesMock.mockClear();
+    generateQuizMock.mockClear();
+    getCategoriesQuestionCountMock.mockClear();
+
+    setActivePinia(createPinia());
+
+    store = useHomeStore();
   });
 
   it('should set correct categories in getCategories method', async () => {
-    const options = [
-      { name: 'All', value: '' },
-      {
-        value: '1',
-        name: 'category 1'
-      },
-      {
-        value: '2',
-        name: 'category 2'
-      }
-    ];
+    const options = mapCategoriesToOptions(categoriesMock);
 
     await store.getCategories();
 
@@ -49,5 +52,48 @@ describe('HomeStore tests', () => {
     expect(store.questionCategories).toStrictEqual(options);
 
     expect(store.isLoading).toBe(false);
+  });
+
+  it('should set emptyCategories array by default', async () => {
+    getCategoriesMock.mockResolvedValue(null);
+
+    await store.getCategories();
+
+    await flushPromises();
+
+    expect(store.questionCategories).toStrictEqual([
+      {
+        name: 'All',
+        value: ''
+      }
+    ]);
+  });
+
+  it('should get categoriesQuestionCount correctly', async () => {
+    await store.getCategoryQuestionsCount('');
+
+    await flushPromises();
+
+    expect(store.categoryQuestionsCount).toStrictEqual(
+      mapCategoriesQuestionCounts(categoriesQuestionCountResponseMock)
+    );
+  });
+
+  it('should get categoriesQuestionCount and not set it if no response', async () => {
+    getCategoriesQuestionCountMock.mockResolvedValue(null);
+
+    await store.getCategoryQuestionsCount('');
+
+    await flushPromises();
+
+    expect(store.categoryQuestionsCount).toStrictEqual(
+      defaultQuestionsCountValue
+    );
+  });
+
+  it('should return id after generating new quiz', async () => {
+    const id = await store.generateQuiz(GenerateQuizDtoMock.create());
+
+    expect(id).toStrictEqual(generateQuizResponseMock.id);
   });
 });
