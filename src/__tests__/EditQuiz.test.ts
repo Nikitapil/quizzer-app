636d63900c90d7cf465521bfa2c61911ed4796e2
Nikitapil <nikitapiliavets@gmail.com>
2024-08-14 20:59:35 +0300

    refactore EditQuiz component tests

--- src/__tests__/EditQuiz.test.ts
+++ src/__tests__/EditQuiz.test.ts
@@ -1,32 +1,37 @@
-import { createTestingPinia } from '@pinia/testing';
+import { createPinia, setActivePinia } from 'pinia';
+import { flushPromises, mount } from '@vue/test-utils';
+
 import { useQuizFormStore } from '@/modules/quizes/store/QuizFormStore';
-import { mount } from '@vue/test-utils';
-import EditQuiz from '@/modules/quizes/pages/EditQuiz.vue';
 import { useAuthStore } from '@/modules/auth/store/AuthStore';
+
 import QuizForm from '@/modules/quizes/components/quiz-form/QuizForm.vue';
-import { UserRolesEnum } from '@/api/swagger/Auth/data-contracts';
+import EditQuiz from '@/modules/quizes/pages/EditQuiz.vue';
+import { UserReturnDtoMock } from '@/api/swagger/Auth/mock';
+import { quizApi } from '@/api/apiInstances';
+import { vi } from 'vitest';
+import { SingleQuizReturnDtoMock } from '@/api/swagger/Quizes/mock';
+import router from '@/router';
 
 describe('Edit quiz page test', () => {
-  it('should render loader if isQuizLoading', () => {
-    const pinia = createTestingPinia();
-    const store = useQuizFormStore(pinia);
-    const authStore = useAuthStore(pinia);
-    authStore.user = {
-      id: 1,
-      username: 'Test user',
-      email: 'test@test.test',
-      role: UserRolesEnum.User
-    };
+  const user = UserReturnDtoMock.create();
+  const getQuizMock = vi.fn();
+  const editQuizMock = vi.fn();
+  quizApi.getQuiz = getQuizMock;
+  quizApi.editQuiz = editQuizMock;
 
-    store.getQuizForm = async () => {};
+  beforeEach(() => {
+    getQuizMock.mockReset();
+    setActivePinia(createPinia());
+    const authStore = useAuthStore();
+    authStore.user = user;
+  });
+
+  it('should render loader if isQuizLoading', () => {
+    const store = useQuizFormStore();
 
     store.isQuizLoading = true;
 
-    const wrapper = mount(EditQuiz, {
-      global: {
-        plugins: [pinia]
-      }
-    });
+    const wrapper = mount(EditQuiz);
 
     const loader = wrapper.find('[data-test="round-loader"]');
 
@@ -34,72 +39,68 @@ describe('Edit quiz page test', () => {
   });
 
   it('should render not found message', () => {
-    const pinia = createTestingPinia();
-    const store = useQuizFormStore(pinia);
-    const authStore = useAuthStore(pinia);
-    authStore.user = {
-      id: 1,
-      username: 'Test user',
-      email: 'test@test.test',
-      role: UserRolesEnum.User
-    };
-
-    store.getQuizForm = async () => {};
-
-    const wrapper = mount(EditQuiz, {
-      global: {
-        plugins: [pinia]
-      }
-    });
+    const wrapper = mount(EditQuiz);
 
     const notFound = wrapper.find('[data-test="not-found"]');
 
     expect(notFound.exists()).toBe(true);
   });
 
-  it('should render quizForm', () => {
-    const pinia = createTestingPinia();
-    const store = useQuizFormStore(pinia);
-    const authStore = useAuthStore(pinia);
-    authStore.user = {
-      id: 1,
-      username: 'Test user',
-      email: 'test@test.test',
-      role: UserRolesEnum.User
-    };
+  it('should render quizForm', async () => {
+    getQuizMock.mockResolvedValue(
+      SingleQuizReturnDtoMock.create({
+        canEdit: true
+      })
+    );
 
-    store.getQuizForm = async () => {};
+    const wrapper = mount(EditQuiz);
 
-    store.quizForm = {
-      name: '',
-      isPrivate: false,
-      questions: [
-        {
-          question: '',
-          correctAnswer: '',
-          incorrectAnswers: ['']
-        },
-        {
-          question: '',
-          correctAnswer: '',
-          incorrectAnswers: ['']
-        },
-        {
-          question: '',
-          correctAnswer: '',
-          incorrectAnswers: ['']
-        }
-      ]
-    };
+    await flushPromises();
 
-    const wrapper = mount(EditQuiz, {
-      global: {
-        plugins: [pinia]
-      }
-    });
+    const form = wrapper.findComponent(QuizForm);
+    expect(form.exists()).toBe(true);
+  });
+
+  it('should not redirect to user quizes if not editted after submit', async () => {
+    const routerSpy = vi.spyOn(router, 'push');
+    getQuizMock.mockResolvedValue(
+      SingleQuizReturnDtoMock.create({
+        canEdit: true
+      })
+    );
+
+    const wrapper = mount(EditQuiz);
+
+    await flushPromises();
 
     const form = wrapper.findComponent(QuizForm);
 
-    expect(form.exists()).toBe(true);
+    await form.trigger('submit');
+
+    await flushPromises();
+
+    expect(routerSpy).not.toHaveBeenCalled();
+  });
+
+  it('should redirect to user quizes if editted after submit', async () => {
+    const routerSpy = vi.spyOn(router, 'push');
+    getQuizMock.mockResolvedValue(
+      SingleQuizReturnDtoMock.create({
+        canEdit: true
+      })
+    );
+    editQuizMock.mockResolvedValue(true);
+
+    const wrapper = mount(EditQuiz);
+
+    await flushPromises();
+
+    const form = wrapper.findComponent(QuizForm);
+
+    await form.trigger('submit');
+
+    await flushPromises();
+
+    expect(routerSpy).toHaveBeenCalled();
   });
 });
